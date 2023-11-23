0b40d2242ce3bbf9b1ecf26678c3ba60f78d9de8
liadk07 <91607695+liad07@users.noreply.github.com>
2023-11-23 17:53:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,29 @@ document.addEventListener('DOMContentLoaded', function() {
       maximumAge: 30000,
       timeout: 27000
     });
+
+    // Update speed every 0.1 seconds (100 milliseconds)
+    setInterval(updateSpeed, 100);
   } else {
     alert('Geolocation is not supported by this browser.');
   }
 });
 
-function updateSpeed(position) {
-  const speedElement = document.getElementById('speed');
+function updateSpeed() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    const speedElement = document.getElementById('speed');
 
-  if (position.coords.speed !== null) {
-    const speed = position.coords.speed * 3.6; // Convert m/s to km/h
-    speedElement.textContent = speed.toFixed(2);
-  } else {
-    speedElement.textContent = 'N/A';
-  }
+    if (position.coords.speed !== null) {
+      const speed = position.coords.speed * 3.6; // Convert m/s to km/h
+      speedElement.textContent = speed.toFixed(2);
+    } else {
+      speedElement.textContent = 'N/A';
+    }
+  }, handleGeolocationError, {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 27000
+  });
 }
 
 function handleGeolocationError(error) {
